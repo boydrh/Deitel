cea7e7d9d42dc601c710c7152e46696d60558847
Richard Boyd <boydrh@gmail.com>
2016-05-16 21:46:10 -0400

    update to WhileCounter.java

--- src/chapter05/WhileCounter.java
+++ src/chapter05/WhileCounter.java
@@ -1,5 +1,15 @@
 package chapter05;
 
 public class WhileCounter {
-
+	
+	public static void main(String[] args){
+		int counter = 1;
+		
+		while (counter <=10){
+			System.out.printf("%d ", counter);
+			++counter;
+		}
+		
+		System.out.println();
+	}
 }
