c5f4062a73de30620fbca7ecaa925951f89f05f0
Richard Boyd <boydrh@gmail.com>
2016-05-18 06:26:55 -0400

    added smiley face java GUI

---
+++ src/gui/DrawSmiley.java
@@ -0,0 +1,28 @@
+package gui;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawSmiley extends JPanel{
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		//Draw Face
+		g.setColor(Color.YELLOW);
+		g.fillOval(10, 10, 200, 200);
+		
+		//Draw Eyes
+		g.setColor(Color.BLACK);
+		g.fillOval(55, 65, 30, 30);
+		g.fillOval(135, 65, 30, 30);
+		
+		//Draw Mouth
+		g.fillOval(50, 110, 120, 60);
+		
+		//Touch up Mouth
+		g.setColor(Color.YELLOW);
+		g.fillRect(50, 110, 120, 30);
+		g.fillOval(50, 120, 120, 40);
+	}
+}
